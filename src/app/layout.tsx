626452e109202cb9ef5a37fd1e3b516b676ed916
joanpurbaa <joanpurba562@gmail.com>
2025-02-26 18:41:41 +0700

    fix : fix open graph image preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,8 @@ export const metadata: Metadata = {
 		title: "PlanMate",
 		description: "Atur jadwal tanpa ribet!",
 		creators: "Joan Orlando Purba",
-		images: "./banner.png",
+		images:
+			"https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png",
 		url: "https://plan-mate-joan-punya.vercel.app/",
 	},
 };
@@ -30,16 +31,40 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<meta property="og:image" content="<generated>" />
-				<meta property="og:image:type" content="<generated>" />
-				<meta property="og:image:width" content="<generated>" />
-				<meta property="og:image:height" content="<generated>" />
+				<meta
+					property="og:image"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
+				<meta
+					property="og:image:type"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
+				<meta
+					property="og:image:width"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
+				<meta
+					property="og:image:height"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
 				<meta property="og:image:alt" content="PlanMate" />
 
-				<meta name="twitter:image" content="<generated>" />
-				<meta name="twitter:image:type" content="<generated>" />
-				<meta name="twitter:image:width" content="<generated>" />
-				<meta name="twitter:image:height" content="<generated>" />
+				<meta
+					name="twitter:image"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
+				<meta
+					name="twitter:image:type"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
+				<meta
+					name="twitter:image:width"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
+				<meta
+					name="twitter:image:height"
+					content="https://raw.githubusercontent.com/joanpurbaa/PlanMate/refs/heads/main/public/banner.png"
+				/>
 				<meta property="twitter:image:alt" content="PlanMate" />
 			</head>
 			<body className={`${poppins.className} antialiased bg-zinc-900`}>
